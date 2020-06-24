9fa2066415d9e1dafe2f1e7aabd1cba819e60146
Austin Lindenberg <alindenberg8@gmail.com>
2020-06-24 07:50:16 -0700

    Initial project setup with auth skeleton

--- src/main.js
+++ src/main.js
@@ -1,8 +1,38 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+
+import Profile from './views/Profile'
+import Home from './views/Home'
+
+import { domain, clientId } from "../auth-config.json";
+import { Auth0Plugin } from "./auth";
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/profile", component: Profile}
+]
+let router = new VueRouter({
+  mode: 'history',
+  routes
+})
+Vue.use(VueRouter)
+// Install the authentication plugin here
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: appState => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  }
+});
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
